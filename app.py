ace309ad82decef645e526735f78c82ae69c09fd
unknown <abbacysalman@gmail.com>
2022-02-22 16:08:47 +0500

    flask-todo-app deployment

---
+++ app.py
@@ -0,0 +1,69 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class TODO(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    desc = db.Column(db.String(500), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno}-{self.desc}"
+
+
+
+
+@app.route('/',  methods = ['POST','GET'])
+def hello_world():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = TODO(title = title, desc= desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodo = TODO.query.all()
+    return render_template('index.html', allTodo= allTodo)
+    # return 'Hello_world'
+
+@app.route('/show')
+def products():
+    allTodo = TODO.query.all()
+    # print(alltodo)
+    return "this is my products page"
+
+
+@app.route('/update/<int:sno>',  methods = ['POST','GET'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = TODO.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        todo = TODO.query.filter_by(sno=sno).first()
+        return redirect("/")
+    
+    todo = TODO.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo = todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = TODO.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug= True)
